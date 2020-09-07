693e9613babb4a2945e5e9b6338113f2cbeb5c1a
idolusky <idoluski@gmail.com>
2020-09-07 13:36:32 +0300

    up until the exercises

--- dist/main.js
+++ dist/main.js
@@ -1,28 +1,50 @@
 const source = $("#wonders-template").html()
 const template = Handlebars.compile(source)
 
-const render = function(wonders){
+const render = function (wonders) {
     $("#wonders").empty()
-    let newHtml = template({wonders})
+    let newHtml = template({ wonders })
     $("#wonders").append(newHtml)
 }
 
-const fetch = function(){
-    $.get("/wonders", function(response){
+const fetch = function () {
+    $.get("/wonders", function (response) {
         render(response)
     })
 }
 
-const addWonder = function(){
+const addWonder = function () {
     let newWonder = $("#new-wonder-input").val()
     let newLocation = $("#new-location-input").val()
     //POST the newWonder to the server
+    let data = { name: newWonder, location: newLocation }
+    $.post('/wonder', data, function (response) {
+        console.log("POST complete")
+        fetch()
+    })
+}
+const updateVisited = function (wonder) {
+    $.ajax({
+        url: `wonder/${wonder}`,
+        method: "PUT",
+        success: function (response) {
+            console.log("PUT complete")
+        }
+    })
 }
 
-$("#wonders").on("click", ".visit", function(){
+
+$("#wonders").on("click", ".visit", function () {
     let wonder = $(this).closest(".wonder").find(".name").text()
     //PUT this to the server: update the wonder's `visited` status to `true`
+    updateVisited(wonder.split("-")[0].trim())
+    fetch()
 })
 
 
 fetch() //load the data on page load
+$.ajax({
+    url: '/wonder/Colosseum',
+    method: "DELETE",
+    success: function () { }
+})
---
+++ server/routes/api.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const router = express.Router()
+
+const wonders = [
+    { name: "Mount Everest", location: "Nepal", visited: false },
+    { name: "Grand Canyon", location: "Arizona", visited: false },
+    { name: "Botanical Gardens", location: "Singapore", visited: true },
+    { name: "Pantheon", location: "Greece", visited: false },
+    { name: "Colosseum", location: "Italy", visited: true }
+]
+
+router.get('/wonders', function (req, res) {
+    res.send(wonders)
+})
+
+router.post('/wonder', function (req, res) {
+    console.log("Someone's trying to make a post request")
+    console.log(req.body);
+    wonders.push({ ...req.body, visited: false })
+    res.end()
+})
+
+router.put('/wonder/:name', function (req, res) {
+    console.log("About to update someone")
+    console.log(req.params.name);
+    let wonder = req.params.name
+    wonders.find(w => w.name === wonder).visited = true
+    res.end()// don't forget to end the cycle!
+})
+
+router.delete('/wonder/:name', function (req, res) {
+    let wonder = req.params.name
+    let w = wonders.indexOf(wonder)
+    wonders.splice(w, 1)
+    res.end()
+})
+
+
+module.exports = router
